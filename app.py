c9fcf7b35f65cc15dbf1e40b3c3252f329e2f681
shootylife <a2x.lucas59@gmail.com>
2020-10-27 14:52:07 +0100

    test

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+import qrcode
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/qrcode')
+def hello():
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data('google.fr')
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save('./static/qrcode.png')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
