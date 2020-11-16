87ca2af75fe6685a4593ef88939db4c7dff6d696
shootylife <a2x.lucas59@gmail.com>
2020-11-16 11:45:27 +0100

    feat/getIP

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import secrets
 
 from flask import Flask, render_template
+from flask import request
+from flask import jsonify
+
 import qrcode
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return secrets.token_urlsafe()
+
 
 @app.route('/qrcode')
 def hello():
@@ -22,6 +27,14 @@ def hello():
     img = qr.make_image(fill_color="black", back_color="white")
     img.save('./static/qrcode.png')
     return render_template('index.html')
+
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        return jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
+    else:
+        return jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR']}), 200
 
 
 if __name__ == '__main__':
