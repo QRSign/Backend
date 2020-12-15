06eaca2b00b0dcd7e16247e425f9f233e20bf552
Louis <louis.dutoitld@gmail.com>
2020-12-15 09:14:44 +0100

    :construction: API key

--- app/router.py
+++ app/router.py
@@ -1,11 +1,47 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 app = Flask(__name__)
 
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # A modifier
+jwt = JWTManager(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    if username != 'test' or password != 'test':
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token), 200
+
+
+# Protect a view with jwt_required, which requires a valid access token
+# in the request to access.
+@app.route('/protected', methods=['GET'])
+@jwt_required
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 
 
