9d01327d8d0a78db9a9cc58b05e2d5c78580566b
Pierre FONTAINE <pierrefontaine00@gmail.com>
2020-12-15 08:35:41 +0100

    :bug: Fix endpoints for REST API.

--- app/dao/entities/qrcode.py
+++ app/dao/entities/qrcode.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
-
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from .entity import Entity, Base
 
 
@@ -9,8 +8,24 @@ class Qrcode(Entity, Base):
     title = Column(String)
     token = Column(String)
     created_by = Column(Integer, ForeignKey('user.id'))
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
 
-    def __init__(self, title, token, created_by):
+    def __init__(self, title, token, created_by, start_time, end_time):
+        Entity.__init__(self)
         self.title = title
         self.token = token
         self.created_by = created_by
+        self.start_time = start_time
+        self.end_time = end_time
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'token': self.token,
+            'user': self.created_by,
+            'start_time': self.start_time,
+            'end_time': self.end_time
+        }
